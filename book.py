1d506407fc1f8d423a16e42f2473b0e63affe5c4
Mejbaur Bahar Fagun <fagun115946@gmail.com>
2021-07-08 22:51:41 +0600

    Add files via upload

---
+++ book.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+books_filename = 'BX-Books.csv'
+ratings_filename = 'BX-Book-Ratings.csv'
+# import csv data into dataframes
+df_books = pd.read_csv(
+    books_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['isbn', 'title', 'author'],
+    usecols=['isbn', 'title', 'author'],
+    dtype={'isbn': 'str', 'title': 'str', 'author': 'str'})
+
+df_ratings = pd.read_csv(
+    ratings_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['user', 'isbn', 'rating'],
+    usecols=['user', 'isbn', 'rating'],
+    dtype={'user': 'int32', 'isbn': 'str', 'rating': 'float32'})
